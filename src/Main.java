7bd6820824d2163053d6e7bae78c4638a11246ee
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-27 14:31:23 +0100

    Get part of string, when greater than 3 characters

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import javax.swing.*;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+        partialString("Manuel");
+    }
+
+    public static void partialString(String a)
+    {
+
+        int stringLen = a.length();
+
+        if(stringLen < 3){
+            JOptionPane.showMessageDialog(null, "Sie haben weniger als 3 Zeichen angegeben!");
+        } else {
+            System.out.println("Die 2. Hälfte des Strings lautet: " + a.substring(stringLen / 2));
+        }
 
     }
 
